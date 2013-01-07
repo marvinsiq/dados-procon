eb4eb7d4f91502f891ee9f6b02dc838741a15012
aureliano <aureliano.franca@hotmail.com>
2013-01-07 18:22:02 -0200

    Cria página de perfil da pessoa jurídica.

--- app/controllers/pessoa_juridica.rb
+++ app/controllers/pessoa_juridica.rb
@@ -3,6 +3,7 @@ DadosProcon.controllers :pessoa_juridica do
   get :profile, :map => '/pessoajuridica/:id' do
     @pessoa_juridica = PessoaJuridica.where('_id' => params[:id]).first
     redirect '/404' unless @pessoa_juridica
+    @ranking = prepare_ranking @pessoa_juridica
     
     @breadcrumbs = [:text => 'Not implemented yet', :link => '/']
     render 'pessoa_juridica/profile'
--- app/helpers/pessoa_juridica_helper.rb
+++ app/helpers/pessoa_juridica_helper.rb
@@ -3,5 +3,20 @@ DadosProcon.helpers do
   def pessoa_juridica_name(pessoa_juridica)
     return '' if pessoa_juridica.nil?
     (pessoa_juridica.nome_fantasia.nil? || pessoa_juridica.nome_fantasia == 'NULL') ? pessoa_juridica.razao_social : pessoa_juridica.nome_fantasia
-  end  
+  end
+  
+  def prepare_ranking(pessoa_juridica)
+    ranking = pessoa_juridica.ranking ||= Ranking.new
+    ranking.reclamacoes_total ||= 0
+    ranking.reclamacoes_2009 ||= 0
+    ranking.reclamacoes_2010 ||= 0
+    ranking.reclamacoes_2011 ||= 0
+    
+    ranking
+  end
+  
+  def format_position(position)
+    return 'Sem ranking' if position.nil? || position == 0
+    "#{position} &#186;"
+  end
 end
