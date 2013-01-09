333e73effbe723a2401120ec5f673282dc28089f
aureliano <aureliano.franca@hotmail.com>
2013-01-09 09:33:45 -0200

    Implementa teste de especificação da rota para reclamações de uma pessoa jurídica.

--- spec/app/controllers/pessoa_juridica_controller_spec.rb
+++ spec/app/controllers/pessoa_juridica_controller_spec.rb
@@ -9,5 +9,11 @@ describe "PessoaJuridicaController" do
     last_response.body.should include '<title>Consumidor online: Pessoa Jurídica</title>'
     last_response.body.should include '<h3>Banco Cem por cento</h3>'
   end
+
+  it "carrega página de reclamações da pessoa jurídica" do
+    get "/pessoajuridica/#{PessoaJuridica.where(:cnpj => '99866532321227').first.id}/reclamacoes"
+    last_response.body.should include '<title>Consumidor online: Pessoa Jurídica - Reclamações</title>'
+    last_response.body.should include '<h3>Banco Cem por cento - Reclamações</h3>'
+  end
   
 end
