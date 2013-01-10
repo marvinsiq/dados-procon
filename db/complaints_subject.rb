63c0441e773653c6afa1949cd4f46e44aa4f2fe0
aureliano <aureliano.franca@hotmail.com>
2013-01-10 14:16:18 -0200

    Corrige povoamento da coleção "reclamacaos".

--- db/complaints_subject.rb
+++ db/complaints_subject.rb
@@ -1,10 +1,10 @@
 require 'yaml'
 
-res = Reclamacao.all.distinct(:codigo_assunto)
+res = Reclamacao.all.distinct(:codigo_problema)
 
 data = {}
 res.each do |codigo|
-  descricao = Reclamacao.where(:codigo_assunto => codigo).only(:descricao_assunto).limit(1).first.descricao_assunto
+  descricao = Reclamacao.where(:codigo_problema => codigo).only(:descricao_problema).limit(1).first.descricao_assunto
   data[codigo] = descricao
 end
 
