7348a15aa851b720919e70b1e25a21b6864d521b
aureliano <aureliano.franca@hotmail.com>
2013-01-11 16:09:55 -0200

    Carrega tipos de problemas fictícios no ambiente de teste.

--- config/boot.rb
+++ config/boot.rb
@@ -3,7 +3,10 @@ require 'yaml'# Defines our constants
 PADRINO_ENV  = ENV["PADRINO_ENV"] ||= ENV["RACK_ENV"] ||= "development"  unless defined?(PADRINO_ENV)
 PADRINO_ROOT = File.expand_path('../..', __FILE__) unless defined?(PADRINO_ROOT)
 STOPWORDS = File.read('stopwords').split "\n"
-COMPLAINT_PROBLEMS = YAML.load_file('.complaint_problems.yml') if File.exists? '.complaint_problems.yml'
+COMPLAINT_PROBLEMS = case PADRINO_ENV
+  when 'teste' then {1 => 'teste'}
+  else YAML.load_file('.complaint_problems.yml') if File.exists? '.complaint_problems.yml'
+end
 
 # Load our dependencies
 require 'rubygems' unless defined?(Gem)
